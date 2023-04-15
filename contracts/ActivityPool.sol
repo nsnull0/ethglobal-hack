8545e7d5225dfc2e2da743db68ff2eaeaa622487
yosephws <yoseph.savianto@paypay-corp.co.jp>
2023-04-15 12:35:11 +0900

    [modify] activitypool completion

--- contracts/ActivityPool.sol
+++ contracts/ActivityPool.sol
@@ -1,35 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-interface Activity {
+import "./utils/SafeERC20.sol";
+import "./utils/ReentrancyGuard.sol";
+
+interface IActivity {
   function getName() external returns (string memory);
+  function subscribeToTheEvent() external;
 }
 
-interface IERC20 {
-  function transferFrom(address from, address to, uint256 amount) external returns (bool);
-  function allowance(address owner, address spender) external view returns (uint256);
-}
-
-contract ActivityPool {
+contract ActivityPool is ReentrancyGuard {
   
   address owner;
 
-  Activity[] activities;
+  IActivity[] public activities;
 
   constructor() {
     owner = msg.sender;
   }
 
-  function getListOfActivity() external view returns (Activity[] memory) {
+  function getListOfActivity() external view returns (IActivity[] memory) {
     return activities;
   }
 
-  function registerActivity() external {
-    
+  function createActivity() external nonReentrant {
+    IActivity _activity = IActivity(new Activity());
+    activities.push(_activity);
   }
 
   modifier onlyOwner() {
     require((owner == msg.sender), "Callable only owner");
+    _;
+  }
+}
+
+contract Activity is IActivity, ReentrancyGuard {
+  using SafeERC20 for IERC20;
+
+  string name;
+  address owner;
+  IERC20 paymentToken;
+  
+  uint256 amount;
+
+  address[] subscribedPeople;
+
+  event subscribed(string _name, uint256 _timestamp);
+
+  constructor() {
+    owner = msg.sender;
+    amount = 0;
+  }
+
+  function setPrice(
+    uint256 _amount
+  ) external onlyOwner {
+    amount = _amount;
+  }
+
+  function setPaymentToken(
+    address _paymentToken
+  ) external onlyOwner {
+    paymentToken = IERC20(_paymentToken);
+  }
+
+  function getName() external view returns (string memory) {
+    return name;
+  }
+
+  function subscribeToTheEvent() external nonReentrant {
+    require((amount > 0), "Price not set yet");
+    require(
+            IERC20(paymentToken).allowance(msg.sender, address(this)) >=
+                amount,
+            "Insufficient allowance"
+        );
+
+    IERC20(paymentToken).safeTransferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+    IERC20(paymentToken).safeTransfer(owner, amount);
+    
+    emit subscribed(name, block.timestamp);
+  }
+
+  modifier onlyOwner() {
+    require((owner == msg.sender), "Callable: only Owner");
     _;
   }
 }
